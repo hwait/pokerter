86f58e1dc83dea1feb9b90d2565a7eb56a1be36d
Nikolay Kashavkin <hwaitt@gmail.com>
2022-07-01 15:30:35 +0600

    Table init via flatMap

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,10 +4,9 @@ import cards.domain._
 object Main extends ZIOAppDefault {
   override def run =
     (for {
-      table <- Table.init(GameConfig.NLHoldem, 6, Blindes(50, 100, 0))
-      _ <- table.playN(1)
-      // table <- Table.init(GameConfig.Omaha, 4, "PL", 50, 100)
-      // _ <- table.playN(2)
+      _ <- Table
+        .init(GameConfig.NLHoldem, 6, Blindes(50, 100, 0))
+        .flatMap(t => t.playN(2))
     } yield ())
       .provide(
         Clock.live,
